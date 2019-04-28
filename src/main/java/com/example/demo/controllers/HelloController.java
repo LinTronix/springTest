3a3f5a96600dec99b6728b893a05a5619ba5b948
linuxGitJava <tronis.janis@gmail.com>
2019-04-28 11:29:45 +0300

    first test

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,11 +1,27 @@
 package com.example.demo.controllers;
 
+import com.example.demo.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.lang.annotation.Target;
 @Controller
 public class HelloController {
+    @Autowired
+private HelloService helloService;
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld(){
+        return helloService.getHello();
+    }
 
+    @GetMapping("/test")
+    @ResponseBody
+public String test(){
+        return "test";
+    }
 
 
 
