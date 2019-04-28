6268fae951270d84816501c1f07f1f249a792c5e
linuxGitJava <tronis.janis@gmail.com>
2019-04-28 10:56:41 +0300

    init

---
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import java.lang.annotation.Target;
+@Controller
+public class HelloController {
+
+
+
+
+}
